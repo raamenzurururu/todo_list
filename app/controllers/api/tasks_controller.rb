7da2acb4075a5774a0f401a432617efdfce8f076
github raamenzurururu <reku9696@gmail.com>
2020-07-23 18:46:02 +0900

    complate app

---
+++ app/controllers/api/tasks_controller.rb
@@ -0,0 +1,28 @@
+module Api
+
+    class Api::TasksController < ApplicationController
+        skip_before_action :verify_authenticity_token
+
+        def index
+            @tasks = Task.order('created_at DESC')
+        end 
+
+        def create
+            @task = Task.new(task_params)
+
+            if @task.save
+                render json: @task, status: :created
+            else
+                render json: @task.errors, status: :unprocessable_entity
+            end 
+        end
+        
+        def destroy
+            Task.find(params[:id]).destroy!
+        end
+
+        private def task_params
+            params.require(:task).permit(:name, :is_done)
+        end
+    end
+end 
